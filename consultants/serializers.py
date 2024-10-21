dbc114b502db8c23bd56a73bd217877e7181f830
oTKFx1 <124001234+oTKFx1@users.noreply.github.com>
2024-10-21 21:29:08 +0300

    Update serializers.py

--- consultants/serializers.py
+++ consultants/serializers.py
@@ -8,14 +8,34 @@ class ConsultantDocumentSerializer(serializers.ModelSerializer):
         fields = ['id', 'file', 'uploaded_at']
 
 
+class UserSpecialistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username']
+
+class FetchCurrentNormalUserSerializers(serializers.ModelSerializer):
+    user = UserSpecialistSerializer(read_only=True)
+    class Meta:
+        model = NormalUserInfo
+        fields = ['user', 'phone_number']
+
+class SpecialistProfileFetchSerializer(serializers.ModelSerializer):
+    user = UserSpecialistSerializer(read_only=True)  # Use lowercase and read_only if not writable
+
+    class Meta:
+        model = SpecialistProfile
+        fields = ['user', 'picture', 'description', 'phone_number', 'experience_years', 'certificate']
+
 class ConsultantChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = ConsultantChat
         fields = ['id', 'consultant', 'from_user', 'to_user', 'message', 'file', 'read', 'created_at', 'updated_at']
 
 class ConsultantServiceSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(source='user.user.username')  # Assuming NormalUserInfo has a user attribute
-    specialist = serializers.StringRelatedField(source='specialist.specialist_name')  # Adjust according to your field
+    # user = serializers.StringRelatedField(source='user.user.username')
+    user = FetchCurrentNormalUserSerializers(read_only=True)
+    specialist = SpecialistProfileFetchSerializer(read_only=True) # Assuming NormalUserInfo has a user attribute
+    # specialist = serializers.StringRelatedField(source='specialist.specialist_name')  # Adjust according to your field
 
     documents = ConsultantDocumentSerializer(many=True, read_only=True)  # Nested serializer for documents
     chats = ConsultantChatSerializer(many=True, read_only=True)  # Nested serializer for chats
